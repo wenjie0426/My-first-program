c3cb7c724c92d8af34c8c0675717afa4578203ba
Jie <wejie0426@outlook.com>
2021-06-28 12:02:05 +0800

    123test

---
+++ ce.c
@@ -0,0 +1,15 @@
+/**
+ * @File Name: ce.c
+ * @Brief : {Edited by Jie}
+ * @Author : Jie (wenjie0426@outlook.com)
+ * @Version : 1.0
+ * @Creat Date : 2021-05-24
+ * 
+ */
+
+#include <stdio.h>
+int main()
+{
+   printf("123\n");
+   return 0;
+}
